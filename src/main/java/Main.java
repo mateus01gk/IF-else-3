a5f8726d8fa02caac4cb2b27b5c16d561d297fa5
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-06-27 10:34:38 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
